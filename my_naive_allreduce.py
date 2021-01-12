2d042b448945f7e2dc7d30bd1cf57c6e0c76699c
mordehaibenharush <ben-harush@campus.technion.ac.il>
2021-01-12 23:09:26 +0200

    12.01.21

--- my_naive_allreduce.py
+++ my_naive_allreduce.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mpi4py import MPI
 
 
 def allreduce(send, recv, comm, op):
@@ -13,4 +14,16 @@ def allreduce(send, recv, comm, op):
     comm : MPI.Comm
     op : associative commutative binary operator
     """
-    raise NotImplementedError("To be implemented")
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    for i in range(0, size):
+        if i != rank:
+            comm.Isend([send, len(send), MPI.INT], dest=i, tag=1)
+
+    for i in range(0, size):
+        if i != rank:
+            tmp = np.empty_like(send)
+            comm.Recv(tmp, source=i, tag=1)
+            recv = op(recv, tmp)
+
+    return recv
--- my_ring_allreduce.py
+++ my_ring_allreduce.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mpi4py import MPI
 
 
 def ringallreduce(send, recv, comm, op):
@@ -14,4 +15,29 @@ def ringallreduce(send, recv, comm, op):
     comm : MPI.Comm
     op : associative commutative binary operator
     """
-    raise NotImplementedError("To be implemented")
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    block_size = int(len(send)/size)
+    prv = (rank - 1)
+    if rank == 0:
+        prv = size-1
+    nxt = (rank + 1) % size
+    recv = send
+    tmp = np.empty(block_size)
+
+    for i in range(size):
+        src = (rank-i) if (rank-i) >= 0 else (size-1)
+        comm.Isend([recv[src*block_size:(src+1)*block_size], block_size, MPI.INT], dest=nxt, tag=1)
+        dst = (src-1) if (src-1) >= 0 else (size-1)
+        comm.Recv(tmp, source=prv, tag=1)
+        t = recv[dst*block_size:(dst+1)*block_size]
+        recv[dst*block_size:(dst+1)*block_size] = op(t, tmp)
+
+    for j in range(size):
+        src = ((rank+1)-j) if ((rank+1)-j) >= 0 else (size-1)
+        comm.Isend([recv[src * block_size:(src + 1) * block_size], block_size, MPI.INT], dest=nxt, tag=1)
+        dst = (src-1) if (src-1) >= 0 else (size-1)
+        comm.Recv(tmp, source=prv, tag=1)
+        recv[dst * block_size:(dst + 1) * block_size] = tmp
+
+    return recv
